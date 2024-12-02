66ad5be468d225d7186b924685621bb74d6947a8
2czamica68 <00001094783638sp@al.educacao.sp.gov.br>
2024-12-02 09:12:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    "Qual é a formação da água?",
+    'H2O'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Jamaica?',
+    'A capital da Jamaica é Kingston'
+)
+
+criaCartao(
+    'Química',
+    'Qual é a formação da glicose?',
+    'C6H12O6'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantas capitais tem no Brasil?',
+    '27 capitais'
+)
